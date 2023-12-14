fbb7599aca0904211a0adfbefd2505fce094c956
Takuma-Hiraoka <t-hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2023-12-14 22:15:58 +0900

    add tactile_shm

--- src/CnoidTactileSensorPlugin.cpp
+++ src/CnoidTactileSensorPlugin.cpp
@@ -14,6 +14,9 @@ bool TactileSensorItem::initialize(ControllerIO* io) {
   this->io_ = io;
   this->timeStep_ = io->worldTimeStep();
 
+  int shm_key = 6555;
+  initialize_shm(shm_key);
+
   YAMLReader reader;
   MappingPtr node;
   try {
@@ -22,6 +25,7 @@ bool TactileSensorItem::initialize(ControllerIO* io) {
     this->io_->os() << "\e[0;31m" << "[TactileSensorItem] " << ex.message() <<  "\e[0m" << std::endl;
   }
   if (!loadConfig(node)) this->io_->os() << "\e[0;31m" << "[TactileSensorItem] cannot load config file \e[0m" << std::endl;
+
   return true;
 }
 
@@ -44,16 +48,28 @@ bool TactileSensorItem::control() {
 	continue;
       } else {
         Position positionParent = (this->tactileSensorList[tactile_sensor_id].linkName == collisions[collision_pair_id]->link[0]->name()) ? collisions[collision_pair_id]->link[0]->T() : collisions[collision_pair_id]->link[1]->T();
+        double direction = (this->tactileSensorList[tactile_sensor_id].linkName == collisions[collision_pair_id]->link[0]->name()) ? -1.0 : 1.0; // 向きを正に統一
 	for (int in_sensor_id=0; in_sensor_id<this->tactileSensorList[tactile_sensor_id].positions.size(); in_sensor_id++) {
 	  Vector3 position = positionParent.translation() + positionParent.rotation() * this->tactileSensorList[tactile_sensor_id].positions[in_sensor_id];
 	  for (int collision_id=0; collision_id< collisions[collision_pair_id]->collisions.size(); collision_id++) {
 	    if ((position - collisions[collision_pair_id]->collisions[collision_id].point).norm() < this->tactileSensorList[tactile_sensor_id].radius) {
-	      this->tactileSensorList[tactile_sensor_id].contacts[in_sensor_id] = true;
+              Matrix3 worldSensorRot = positionParent.rotation() * this->tactileSensorList[tactile_sensor_id].rot[in_sensor_id];
+	      this->tactileSensorList[tactile_sensor_id].depthVector[in_sensor_id] = worldSensorRot.inverse() * collisions[collision_pair_id]->collisions[collision_id].normal * collisions[collision_pair_id]->collisions[collision_id].depth * direction;
 	      break;
 	    }
 	  }
 	}
       }
+    }
+  }
+
+  int index = 0;
+  for (int i=0; i<this->tactileSensorList.size(); i++) {
+    for (int j=0; j<this->tactileSensorList[i].depthVector.size(); j++) {
+      t_shm->contact_force[index][0] = this->tactileSensorList[i].depthVector[j][0];
+      t_shm->contact_force[index][1] = this->tactileSensorList[i].depthVector[j][1];
+      t_shm->contact_force[index][2] = this->tactileSensorList[i].depthVector[j][2];
+      index++;
     }
   }
   return true;
@@ -99,17 +115,33 @@ bool TactileSensorItem::loadConfig(Mapping* topNode) {
       Vector3 direction2 = Vector3(direction2Tmp[0].toDouble(), direction2Tmp[1].toDouble(), direction2Tmp[2].toDouble());
       int num_dir1 = info->extract("num_dir1")->toInt();
       int num_dir2 = info->extract("num_dir2")->toInt();
+      auto rot_ = info->extract("rot");
+      auto& rotTmp = *rot_->toListing();
+      Matrix3 rot;
+      rot << rotTmp[0].toDouble(), rotTmp[1].toDouble(), rotTmp[2].toDouble(),
+             rotTmp[3].toDouble(), rotTmp[4].toDouble(), rotTmp[5].toDouble(),
+             rotTmp[6].toDouble(), rotTmp[7].toDouble(), rotTmp[8].toDouble();
       for (int j=0; j < num_dir1; j++) {
 	for (int k=0; k < num_dir2; k++) {
 	  sensor.positions.push_back(point + direction1 * j / num_dir1 + direction2 * k / num_dir2);
-	  sensor.contacts.push_back(false);
+          sensor.rot.push_back(rot);
+	  sensor.depthVector.push_back(Vector3::Zero());
 	}
       }
     }
     this->tactileSensorList.push_back(sensor);
-    this->io_->os() << "\e[0;31m" << "[TactileSensorItem] " << linkName <<  "\e[0m" << std::endl;
+    this->io_->os() << "[TactileSensorItem] senser attached to " << linkName << std::endl;
   }
   return true;
+}
+
+void TactileSensorItem::initialize_shm(int shm_key)
+{
+  t_shm = (struct tactile_shm *) set_shared_memory(shm_key, sizeof(struct tactile_shm));
+  if (t_shm == NULL) {
+    this->io_->os() << "\e[0;31m" << "[TactileSensorItem] set_shared_memory failed" <<  "\e[0m" << std::endl;
+    exit(1);
+  }
 }
 
 class CnoidTactileSensorPlugin : public Plugin
--- src/TactileSensorItem.h
+++ src/TactileSensorItem.h
@@ -2,6 +2,7 @@
 #define TACTILESENSORPLUGIN_H
 
 #include <cnoid/ControllerItem>
+#include "tactile_shm.h"
 
 namespace cnoid {
 
@@ -10,7 +11,8 @@ class TactileSensor
  public:
   std::string linkName;
   std::vector<Vector3> positions; // リンク座標系でどこに取り付けられているか
-  std::vector<bool> contacts;
+  std::vector<Matrix3> rot; // リンク座標系でセンサの姿勢．zがリンク内側方向
+  std::vector<Vector3> depthVector;
   double radius = 0.01; // 接触とみなす半径
 };
 
@@ -30,6 +32,7 @@ class TactileSensor
     virtual bool restore(const Archive& archive) override;
 
     bool loadConfig(Mapping* topNode);
+    void initialize_shm(int shm_key);
 
   protected:
     cnoid::ControllerIO* io_;
@@ -37,6 +40,8 @@ class TactileSensor
     std::vector<TactileSensor> tactileSensorList;
 
     double timeStep_;
+
+    struct tactile_shm *t_shm;
   };
 }
 
